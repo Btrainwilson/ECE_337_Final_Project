db611c74c4f555b1df42f04a27e2cbbac64df716
Jackson Barrett <barret29@purdue.edu>
2018-04-10 15:43:28 -0400

    setup

---
+++ Output_buffer/source/FIFO.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   FIFO.sv
+// Created:     4/10/2018
+// Author:      Jackson Barrett
+// Lab Section: 337-02
+// Version:     1.0  Initial Design Entry
+// Description: FIFO buffer for Ethernet-to-USB packet statisitc Collector
