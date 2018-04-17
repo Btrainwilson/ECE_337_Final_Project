9efeef9d1125e9a4ad4186ff8bdaac5b53947062
Luke Upton <upton0@purdue.edu>
2018-04-17 15:24:15 -0400

    byte_transmitter works in source and mapped

--- Transmitter_Module/do_mapping.tcl
+++ Transmitter_Module/do_mapping.tcl
@@ -1,6 +1,6 @@
 # Step 1:  Read in the source file
-analyze -format sverilog -lib WORK { flex_counter.sv}
-elaborate flex_counter -lib WORK
+analyze -format sverilog -lib WORK {USB_Timer.sv flex_counter.sv flex_pts_sr.sv bit_stuff.sv Byte_Register.sv Encoder.sv byte_transmitter.sv}
+elaborate byte_transmitter -lib WORK
 uniquify
 # Step 2: Set design constraints
 # Uncomment below to set timing, area, power, etc. constraints
@@ -13,12 +13,12 @@ uniquify
 compile -map_effort medium
 
 # Step 4: Output reports
-report_timing -path full -delay max -max_paths 30 -nworst 1 > reports/flex_counter.rep
-report_area >> reports/flex_counter.rep
-report_power -hier >> reports/flex_counter.rep
+report_timing -path full -delay max -max_paths 30 -nworst 1 > reports/byte_transmitter.rep
+report_area >> reports/byte_transmitter.rep
+report_power -hier >> reports/byte_transmitter.rep
 
 # Step 5: Output final VHDL and Verilog files
-write_file -format verilog -hierarchy -output "mapped/flex_counter.v"
+write_file -format verilog -hierarchy -output "mapped/byte_transmitter.v"
 echo "\nScript Done\n"
 echo "\nChecking Design\n"
 check_design
